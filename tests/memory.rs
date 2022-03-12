6350ce2693c3849006d5304e2d6eed21b952b317
Michael Madden <mikemadden42@icloud.com>
2022-03-11 20:52:46 -0600

    Allow for zero swap.

--- tests/memory.rs
+++ tests/memory.rs
@@ -3,5 +3,5 @@ fn test_memory() {
     use sysinfo::SystemExt;
     let s = sysinfo::System::new_all();
     assert!(s.total_memory() > 0);
-    assert!(s.total_swap() > 0);
+    assert!(s.total_swap() >= 0);
 }
