b50f911862e0b9a5cef57fa818f79776723b0067
Michael Madden <mikemadden42@icloud.com>
2022-03-11 19:28:05 -0600

    Use tabled crate to display info in table.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,28 @@
-use sysinfo::{NetworkExt, System, SystemExt};
+use sysinfo::{System, SystemExt};
+use tabled::{Rotate, Style, Table, Tabled};
 
 fn main() {
+    #[derive(Tabled)]
     struct SystemInfo {
         logical_cores: usize,
         physical_cores: usize,
+        total_memory: u64,
+        total_swap: u64,
+        used_memory: u64,
+        used_swap: u64,
     }
 
     let sys = System::new_all();
 
-    // We display the disks:
-    println!("=> disk list:");
-    for disk in sys.disks() {
-        println!("{:?}", disk);
-    }
-
-    // Network data:
-    for (interface_name, data) in sys.networks() {
-        println!(
-            "{}: {}/{} B",
-            interface_name,
-            data.received(),
-            data.transmitted()
-        );
-    }
-
-    // Components temperature:
-    for component in sys.components() {
-        println!("{:?}", component);
-    }
-
-    // Memory information:
-    println!("total memory: {} KB", sys.total_memory());
-    println!("used memory : {} KB", sys.used_memory());
-    println!("total swap  : {} KB", sys.total_swap());
-    println!("used swap   : {} KB", sys.used_swap());
-
-    let system_info = SystemInfo {
+    let stats = vec![SystemInfo {
         logical_cores: sys.processors().len(),
         physical_cores: sys.physical_core_count().unwrap(),
-    };
+        total_memory: sys.total_memory(),
+        total_swap: sys.total_swap(),
+        used_memory: sys.used_memory(),
+        used_swap: sys.used_swap(),
+    }];
 
-    println!("Number of cores: {}", system_info.logical_cores);
-    println!(
-        "Number of physical processors: {:?}",
-        system_info.physical_cores
-    );
+    let table = Table::new(&stats).with(Rotate::Left).with(Style::modern());
+    println!("{table}");
 }
