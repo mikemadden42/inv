8d73286557aeaf775a91a8a0ea706c92994d9484
Michael Madden <mikemadden42@icloud.com>
2022-12-05 20:43:10 -0600

    Remove tabled.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,27 @@
 use sysinfo::{System, SystemExt};
-use tabled::{Rotate, Style, Table, Tabled};
 
 fn main() {
-    #[derive(Tabled)]
-    struct SystemInfo {
-        logical_cores: usize,
-        physical_cores: usize,
-        total_memory: u64,
-        total_swap: u64,
-        used_memory: u64,
-        used_swap: u64,
-        sys_name: String,
-        sys_kernel_version: String,
-        sys_os_version: String,
-        sys_host_name: String,
-    }
-
     let sys = System::new_all();
 
-    let stats = vec![SystemInfo {
-        logical_cores: sys.cpus().len(),
-        physical_cores: sys.physical_core_count().unwrap(),
-        total_memory: sys.total_memory(),
-        total_swap: sys.total_swap(),
-        used_memory: sys.used_memory(),
-        used_swap: sys.used_swap(),
-        sys_name: sys.name().unwrap(),
-        sys_kernel_version: sys.kernel_version().unwrap(),
-        sys_os_version: sys.os_version().unwrap(),
-        sys_host_name: sys.host_name().unwrap(),
-    }];
+    println!("system hostname: {}", sys.host_name().unwrap());
+    println!("system type: {}", sys.name().unwrap());
+    println!("system os: {}", sys.os_version().unwrap());
+    println!("system kernel: {}", sys.kernel_version().unwrap());
 
-    let mut binding = Table::new(&stats);
-    let table = binding.with(Rotate::Left).with(Style::modern());
-    println!("{table}");
+    // Number of processors
+    println!("Number of cores: {}", sys.cpus().len());
+    println!(
+        "Number of physical processors: {:?}",
+        sys.physical_core_count().unwrap()
+    );
 
-    println!("components:");
+    // Memory information:
+    println!("total memory: {} KB", sys.total_memory());
+    println!("used memory : {} KB", sys.used_memory());
+    println!("total swap  : {} KB", sys.total_swap());
+    println!("used swap   : {} KB", sys.used_swap());
+
+    // Components temperature:
     for component in sys.components() {
         println!("{:?}", component);
     }
